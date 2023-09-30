cc2a2cfa9a0e12897a0c000cea8801010dca9cb4
Chethiya Tharinda Palliyaguruge <ctharinda4114@gmail.com>
2023-10-01 10:13:29 +1300

    Masterdata service cleanup

--- src/main/java/com/chethiya/masterdata/dao/repository/GenderRepository.java
+++ src/main/java/com/chethiya/masterdata/dao/repository/GenderRepository.java
@@ -1,7 +1,6 @@
 package com.chethiya.masterdata.dao.repository;
 
 import com.chethiya.masterdata.model.Gender;
-import com.chethiya.masterdata.model.WorkGrade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/chethiya/masterdata/services/TypeOfWorkService.java
+++
@@ -1,9 +0,0 @@
-package com.chethiya.masterdata.services;
-
-
-import com.chethiya.masterdata.dto.ComplexionDTO;
-import com.chethiya.masterdata.dto.TypeOfWorkDTO;
-
-public interface TypeOfWorkService extends CommonService<TypeOfWorkDTO, String> {
-
-}
